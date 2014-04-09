b8b954ced7694828d8e79fbe43b469530147ac62
peter-shi <shipeng@camera360.com>
2014-04-09 11:42:40 +0800

    safasd

---
+++ testasftas/testasftas/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  testasftas
+//
+//  Created by peter.shi on 14-4-9.
+//  Copyright (c) 2014年 peter.shi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ testasftas/testasftas/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  testasftas
+//
+//  Created by peter.shi on 14-4-9.
+//  Copyright (c) 2014年 peter.shi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
